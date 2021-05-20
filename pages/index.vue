f9ed1d9aa82cdf017f91b306b8cfd0e8db32eed1
Paul Anthony McGowan <xhostcom@gmail.com>
2021-05-20 09:56:20 +0100

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="container">
+    <Header />
+    <h2>Click Users in nav to enter any Random User ID</h2>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
